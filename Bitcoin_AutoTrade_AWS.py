dce84671aae6e504f1a4c8b233946cce0aa016ab
kwangwon83 <33193336+kwangwon83@users.noreply.github.com>
2021-04-28 22:39:19 +0900

    Add files via upload

--- Bitcoin_AutoTrade_AWS.py
+++ Bitcoin_AutoTrade_AWS.py
@@ -47,22 +47,26 @@ send_log_to_slack('Home Desktop', 'Autotrade started')
 # 자동매매 시작, !모든 암호화폐를 원화로 매도하여 준비되었는지 확인!
 time_delay = 30 # 기본 오전 9시로부터 지연시간(분) 설정
 krw = get_balance("KRW")
+if krw is None: krw = 0
+alive_alarm = 0
 while True:
     try:
         now = datetime.datetime.now()
         start_time = get_start_time("KRW-BTC") + datetime.timedelta(minutes=time_delay)
         end_time = start_time + datetime.timedelta(days=1)
 
-        if start_time < now < end_time - datetime.timedelta(seconds=10):
+        if start_time < now < end_time - datetime.timedelta(minutes=5):
             # KRW-BTC
             target_price_BTC = get_target_price("KRW-BTC", 0.5)
             current_price_BTC = get_current_price("KRW-BTC")
             btc = get_balance("BTC")
+            if btc is None: btc = 0
 
             # KRW-ETH
             target_price_ETH = get_target_price("KRW-ETH", 0.5)
             current_price_ETH = get_current_price("KRW-ETH")
             eth = get_balance("ETH")
+            if eth is None: eth = 0
 
             if (target_price_BTC <= current_price_BTC) and (btc == 0):
                 buykrw = krw / 2
@@ -82,29 +86,38 @@ while True:
 
         else:
             btc = get_balance("BTC")
+            if btc is None: btc = 0
             current_price_BTC = get_current_price("KRW-BTC")
             btc_balance_as_krw = btc * current_price_BTC
 
             eth = get_balance("ETH")
+            if eth is None: eth = 0
             current_price_ETH = get_current_price("KRW-ETH")
             eth_balance_as_krw = eth * current_price_ETH
 
             if btc_balance_as_krw > 5000:
                 upbit.sell_market_order("KRW-BTC", btc)
                 send_log_to_slack('Home Desktop', f'[KRW-BTC] 매도완료. 매도가 : {current_price_BTC:,.0f}')
-            else:
+            elif btc_balance_as_krw > 0:
                 send_log_to_slack('Home Desktop', '[KRW-BTC] 매도잔고 부족')
 
             if eth_balance_as_krw > 5000:
                 upbit.sell_market_order("KRW-ETH", eth)
                 send_log_to_slack('Home Desktop', f'[KRW-ETH] 매도완료. 매도가 : {current_price_ETH:,.0f}')
-            else:
+            elif eth_balance_as_krw > 0:
                 send_log_to_slack('Home Desktop', '[KRW-ETH] 매도잔고 부족')
 
             krw = get_balance("KRW")
+            if krw is None: krw = 0
 
-        time.sleep(30)
+        if alive_alarm == 360:
+            alive_alarm = 0
+            send_log_to_slack('Home Desktop', f'코인봇 정상작동 중 : {now}')
+        else:
+            alive_alarm = alive_alarm + 1
+
+        time.sleep(10)
 
     except Exception as e:
         print(e)
-        time.sleep(30)
+        time.sleep(10)
